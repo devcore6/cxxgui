2ee2906abfef196445adf46918af6844f139e748
devcore6 <61387985+devcore6@users.noreply.github.com>
2021-11-09 20:27:06 +0100

    Added more symbols

--- src/include/cxxgui/symbols_list.hpp
+++ src/include/cxxgui/symbols_list.hpp
@@ -7,30 +7,44 @@ namespace cxxgui {
     namespace symbols {
 
         /*
-         * ui_base
+         * math
          */
 
-        extern symbol* circle_fill();
+        extern symbol* divide_2_fill();
 
-        extern symbol* circle_inset_thin();
-        extern symbol* circle_inset_extralight();
-        extern symbol* circle_inset_light();
-        extern symbol* circle_inset();
-        extern symbol* circle_inset_medium();
-        extern symbol* circle_inset_semibold();
-        extern symbol* circle_inset_bold();
-        extern symbol* circle_inset_extrabold();
-        extern symbol* circle_inset_black();
+        extern symbol* divide_2_thin();
+        extern symbol* divide_2_extralight();
+        extern symbol* divide_2_light();
+        extern symbol* divide_2();
+        extern symbol* divide_2_medium();
+        extern symbol* divide_2_semibold();
+        extern symbol* divide_2_bold();
+        extern symbol* divide_2_extrabold();
+        extern symbol* divide_2_black();
 
-        extern symbol* circle_thin();
-        extern symbol* circle_extralight();
-        extern symbol* circle_light();
-        extern symbol* circle();
-        extern symbol* circle_medium();
-        extern symbol* circle_semibold();
-        extern symbol* circle_bold();
-        extern symbol* circle_extrabold();
-        extern symbol* circle_black();
+        extern symbol* divide_circle_fill();
+
+        extern symbol* divide_circle_thin();
+        extern symbol* divide_circle_extralight();
+        extern symbol* divide_circle_light();
+        extern symbol* divide_circle();
+        extern symbol* divide_circle_medium();
+        extern symbol* divide_circle_semibold();
+        extern symbol* divide_circle_bold();
+        extern symbol* divide_circle_extrabold();
+        extern symbol* divide_circle_black();
+
+        extern symbol* divide_fill();
+
+        extern symbol* divide_thin();
+        extern symbol* divide_extralight();
+        extern symbol* divide_light();
+        extern symbol* divide();
+        extern symbol* divide_medium();
+        extern symbol* divide_semibold();
+        extern symbol* divide_bold();
+        extern symbol* divide_extrabold();
+        extern symbol* divide_black();
 
         extern symbol* minus_circle_fill();
 
@@ -56,6 +70,42 @@ namespace cxxgui {
         extern symbol* minus_extrabold();
         extern symbol* minus_black();
 
+        extern symbol* multiply_circle_fill();
+
+        extern symbol* multiply_circle_thin();
+        extern symbol* multiply_circle_extralight();
+        extern symbol* multiply_circle_light();
+        extern symbol* multiply_circle();
+        extern symbol* multiply_circle_medium();
+        extern symbol* multiply_circle_semibold();
+        extern symbol* multiply_circle_bold();
+        extern symbol* multiply_circle_extrabold();
+        extern symbol* multiply_circle_black();
+
+        extern symbol* multiply_thin();
+        extern symbol* multiply_extralight();
+        extern symbol* multiply_light();
+        extern symbol* multiply();
+        extern symbol* multiply_medium();
+        extern symbol* multiply_semibold();
+        extern symbol* multiply_bold();
+        extern symbol* multiply_extrabold();
+        extern symbol* multiply_black();
+
+        extern symbol* mulyiply_fill();
+
+        extern symbol* percent_fill();
+
+        extern symbol* percent_thin();
+        extern symbol* percent_extralight();
+        extern symbol* percent_light();
+        extern symbol* percent();
+        extern symbol* percent_medium();
+        extern symbol* percent_semibold();
+        extern symbol* percent_bold();
+        extern symbol* percent_extrabold();
+        extern symbol* percent_black();
+
         extern symbol* plus_circle_fill();
 
         extern symbol* plus_circle_thin();
@@ -79,6 +129,80 @@ namespace cxxgui {
         extern symbol* plus_bold();
         extern symbol* plus_extrabold();
         extern symbol* plus_black();
+
+        /*
+         * ui_base
+         */
+
+        extern symbol* checkmark_circle_fill();
+
+        extern symbol* checkmark_circle_thin();
+        extern symbol* checkmark_circle_extralight();
+        extern symbol* checkmark_circle_light();
+        extern symbol* checkmark_circle();
+        extern symbol* checkmark_circle_medium();
+        extern symbol* checkmark_circle_semibold();
+        extern symbol* checkmark_circle_bold();
+        extern symbol* checkmark_circle_extrabold();
+        extern symbol* checkmark_circle_black();
+
+        extern symbol* checkmark_fill();
+
+        extern symbol* checkmark_thin();
+        extern symbol* checkmark_extralight();
+        extern symbol* checkmark_light();
+        extern symbol* checkmark();
+        extern symbol* checkmark_medium();
+        extern symbol* checkmark_semibold();
+        extern symbol* checkmark_bold();
+        extern symbol* checkmark_extrabold();
+        extern symbol* checkmark_black();
+
+        extern symbol* circle_fill();
+
+        extern symbol* circle_inset_thin();
+        extern symbol* circle_inset_extralight();
+        extern symbol* circle_inset_light();
+        extern symbol* circle_inset();
+        extern symbol* circle_inset_medium();
+        extern symbol* circle_inset_semibold();
+        extern symbol* circle_inset_bold();
+        extern symbol* circle_inset_extrabold();
+        extern symbol* circle_inset_black();
+
+        extern symbol* circle_thin();
+        extern symbol* circle_extralight();
+        extern symbol* circle_light();
+        extern symbol* circle();
+        extern symbol* circle_medium();
+        extern symbol* circle_semibold();
+        extern symbol* circle_bold();
+        extern symbol* circle_extrabold();
+        extern symbol* circle_black();
+
+        extern symbol* cross_circle_fill();
+
+        extern symbol* cross_circle_thin();
+        extern symbol* cross_circle_extralight();
+        extern symbol* cross_circle_light();
+        extern symbol* cross_circle();
+        extern symbol* cross_circle_medium();
+        extern symbol* cross_circle_semibold();
+        extern symbol* cross_circle_bold();
+        extern symbol* cross_circle_extrabold();
+        extern symbol* cross_circle_black();
+
+        extern symbol* cross_fill();
+
+        extern symbol* cross_thin();
+        extern symbol* cross_extralight();
+        extern symbol* cross_light();
+        extern symbol* cross();
+        extern symbol* cross_medium();
+        extern symbol* cross_semibold();
+        extern symbol* cross_bold();
+        extern symbol* cross_extrabold();
+        extern symbol* cross_black();
 
         extern symbol* warning_fill();
 
